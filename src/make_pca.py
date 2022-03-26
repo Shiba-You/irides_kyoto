3bab4555eed8ff2ae93e7c3fc53486dcff6b6aa8
Shiba-You <50415577+Shiba-You@users.noreply.github.com>
2022-03-26 14:28:23 +0900

    feat: グラフ範囲の指定

--- src/make_pca.py
+++ src/make_pca.py
@@ -25,6 +25,7 @@ class make_pca:
     self.df = pd.DataFrame()
     self.dfs = pd.DataFrame()
     self.feature = []
+    self.f_len = 0
     self.key = []
 
   def init_data(self):
@@ -47,78 +48,96 @@ class make_pca:
   def output_to_sheet(self, df, sheet):
     with pd.ExcelWriter(self.output_file_name, mode='a') as writer:
       df.to_excel(writer, sheet_name=sheet)
+  
+  def calc_lim(self, axs):
+    min_x, max_x = axs.get_xlim()
+    min_y, max_y = axs.get_ylim()
+    dx = (max_x - min_x) / 100
+    dy = (max_y - min_y) / 100
+    return dx, dy
 
-  def make_scatter(self, col):
-    pdf = PdfPages(self.output_chart_path+"_主成分プロット.pdf")
+  def make_scatter(self):
+    pdf = PdfPages(self.output_chart_path+"_主成分散布図.pdf")
     i = 0
     group = self.df.iloc[:, 1] == "A"
     feature_A = self.feature[[group]]
     feature_B = self.feature[[np.logical_not(group)]]
-    for pc_f, pc_s in itertools.combinations(range(len(self.feature)), 2):
-      if i%8==0:
+    for pc_f, pc_s in itertools.combinations(range(self.f_len), 2):
+      if i%2==0:
         if i != 0:
           pdf.savefig()
           plt.clf()
         i = 0
-        f, axs = plt.subplots(2, 4, figsize=(20, 10))
+        f, axs = plt.subplots(1, 2, figsize=(16, 8))
         plt.subplots_adjust(wspace=0.4, hspace=0.8, bottom=0.17, top=0.93)
+        dx, dy = self.calc_lim(axs[i%2])
       for x, y, idx, gender in zip(self.feature[:, pc_f], self.feature[:, pc_s], self.df.iloc[:, 0], self.df.iloc[:, 2]):
-        axs[i//4, i%4].text(x, y, str(idx)+"_"+gender, fontsize=5)
-      axs[i//4, i%4].scatter(feature_A[:, pc_f], feature_A[:, pc_s], alpha=0.8, s=5, c="red", label="A")
-      axs[i//4, i%4].scatter(feature_B[:, pc_f], feature_B[:, pc_s], alpha=0.8, s=5, c="blue", label="B")
-      axs[i//4, i%4].legend()
-      axs[i//4, i%4].grid()
-      axs[i//4, i%4].set_xlabel("PC" + str(pc_f+1))
-      axs[i//4, i%4].set_ylabel("PC" + str(pc_s+1))
+        axs[i%2].text(x-dx, y+dy, str(idx)+"_"+gender, fontsize=5)
+      axs[i%2].scatter(feature_A[:, pc_f], feature_A[:, pc_s], alpha=0.8, s=10, c="red", label="A")
+      axs[i%2].scatter(feature_B[:, pc_f], feature_B[:, pc_s], alpha=0.8, s=10, c="blue", label="B")
+      axs[i%2].legend()
+      axs[i%2].grid()
+      axs[i%2].set_xlabel("PC" + str(pc_f+1))
+      axs[i%2].set_ylabel("PC" + str(pc_s+1))
+      axs[i%2].axis('square')
 
       i += 1
-      if pc_f == len(self.feature)-2 and pc_s == len(self.feature)-1:
+      if pc_f == self.f_len-2 and pc_s == self.f_len-1:
         pdf.savefig()
         plt.clf()
     pdf.close()
 
   def make_ticker(self, pca):
-    plt.gca().get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+    pdf = PdfPages(self.output_chart_path+"_累積寄与率.pdf")
+    plt.figure(figsize=(20,10))
+    axes = plt.gca()
+    axes.get_xaxis().set_major_locator(ticker.MaxNLocator(integer=True))
+    dx, dy = self.calc_lim(axes)
     plt.plot([0] + list( np.cumsum(pca.explained_variance_ratio_)), "-o")
-    for x, y in zip(self.df.iloc[:, 0], [0]+list( np.cumsum(pca.explained_variance_ratio_))):
-      plt.text(x-0.5, y+0.02, x)
+    component_num = self.df.iloc[:, 0].values
+    component_num = np.append(0, component_num)
+    for x, y in zip(component_num, [0]+list(np.cumsum(pca.explained_variance_ratio_))):
+      plt.text(x-dx, y+dy, x)
     plt.xlabel("Number of principal components")
     plt.ylabel("Cumulative contribution rate")
     plt.grid()
-    plt.savefig(self.output_chart_path+"_累積寄与率.png")
-    plt.show()
+    pdf.savefig()
+    plt.clf()
+    pdf.close()
+
   
-  def make_relations(self, pca, feature_len):
+  def make_relations(self, pca):
     pdf = PdfPages(self.output_chart_path+"_寄与度相関.pdf")
     i = 0
-    # for pc_f, pc_s in itertools.combinations(range(feature_len), 2):
-    for pc_f, pc_s in itertools.combinations(range(8), 2):
-      if i%8==0:
+    for pc_f, pc_s in itertools.combinations(range(self.f_len), 2):
+      if i%2==0:
         if i != 0:
           pdf.savefig()
           plt.clf()
         i = 0
-        f, axs = plt.subplots(2, 4, figsize=(20, 10))
+        f, axs = plt.subplots(1, 2, figsize=(20, 10))
         plt.subplots_adjust(wspace=0.4, hspace=0.8, bottom=0.17, top=0.93)
-      
+      dx, dy = self.calc_lim(axs[i%2])
       for x, y, name in zip(pca.components_[pc_f], pca.components_[pc_s], self.dfs.columns):
-        axs[i//4, i%4].text(x, y, name, fontsize=5)
-      axs[i//4, i%4].scatter(pca.components_[pc_f], pca.components_[pc_s], alpha=0.8, s=5)
-      axs[i//4, i%4].grid()
-      axs[i//4, i%4].set_xlabel("PC" + str(pc_f+1))
-      axs[i//4, i%4].set_ylabel("PC" + str(pc_s+1))
+        axs[i%2].text(x-dx, y+dy, name, fontsize=5)
+      axs[i%2].scatter(pca.components_[pc_f], pca.components_[pc_s], alpha=0.8, s=10)
+      axs[i%2].grid()
+      axs[i%2].set_xlabel("PC" + str(pc_f+1))
+      axs[i%2].set_ylabel("PC" + str(pc_s+1))
 
       i += 1
-      if pc_f == feature_len-2 and pc_s == feature_len-1:
+      if pc_f == self.f_len-2 and pc_s == self.f_len-1:
         pdf.savefig()
         plt.clf()
     pdf.close()
   
-  def make_eigenvector_ticker(self, pca):
-    pdf = PdfPages(self.output_chart_path+"_固有ベクトルの累積寄与率.pdf")
+  def make_eigenvector_ticker(self, pca, lim=False):
+    if lim:
+      pdf = PdfPages(self.output_chart_path+"_固有ベクトルの累積寄与率（y軸固定）.pdf")
+    else:
+      pdf = PdfPages(self.output_chart_path+"_固有ベクトルの累積寄与率.pdf")
     i = 0
     for feature_idx in range(len(pca.components_[0])):
-      print(i)
       if i%2==0:
         if i != 0:
           pdf.savefig()
@@ -131,11 +150,17 @@ class make_pca:
       for idx, val in enumerate(pca.components_[:, feature_idx]):
         contribution_rate.append(contribution_rate[idx]+val**2)
       axs[i%2].plot(contribution_rate, "-o")
-      for x, y in zip(self.df.iloc[:, 0], contribution_rate):
-        axs[i%2].text(x-0.5, y+0.02, x)
+
+      component_num = self.df.iloc[:, 0].values
+      component_num = np.append(0, component_num)
+      dx, dy = self.calc_lim(axs[i%2])
+      for x, y in zip(component_num, contribution_rate):
+        axs[i%2].text(x-dx, y+dy, x)
       axs[i%2].set_xlabel("Number of principal components")
-      axs[i%2].set_ylabel("Cumulative contribution rate")
+      axs[i%2].set_ylabel("Cumulative contribution rate of " + self.df.columns[feature_idx])
       axs[i%2].grid()
+      if lim:
+        axs[i%2].set_ylim(0, 0.2)
       i += 1
       if feature_idx == len(pca.components_[0])-1:
         pdf.savefig()
@@ -148,38 +173,42 @@ class make_pca:
     n_features    : 特徴量数
     n_components  : 主成分数
     '''
-    pca = PCA()
+    pca = PCA(n_components=4)
     pca.fit(self.dfs)
     self.feature = pca.transform(self.dfs)         #? (n_samples, n_features) => (n_samples, n_components): 各サンプルがそれぞれの主成分をどれだけ有しているかを分布する
-    cols = ["PC{}".format(x + 1) for x in range(len(self.feature))]
+    self.f_len = len(self.feature[0])
+    cols = ["PC{}".format(x + 1) for x in range(self.f_len)]
 
-    #! 主成分得点
-    # df = pd.DataFrame(feature, columns=cols)
+    # # ! 主成分得点
+    # df = pd.DataFrame(self.feature, columns=cols)
     # self.output_to_sheet(df, sheet="主成分得点")
 
-    #! 散布図
-    # self.make_scatter(cols)
-
-    #! 寄与率
+    # #! 寄与率
     # df = pd.DataFrame(pca.explained_variance_ratio_, index=cols)
     # self.output_to_sheet(df, sheet="寄与率")
 
-    #! 累積寄与率
-    # self.make_ticker(pca)
-
-    #! 固有値
+    # #! 固有値
     # df = pd.DataFrame(pca.explained_variance_, index=cols)
     # self.output_to_sheet(df, sheet="固有値")
 
-    #! 固有ベクトル
+    # #! 固有ベクトル
     # df = pd.DataFrame(pca.components_, columns=self.dfs.columns, index=cols)
     # self.output_to_sheet(df, sheet="固有ベクトル")
-    
-    #! 固有ベクトルの散布図
-    # self.make_relations(pca, len(self.feature))
 
-    #! 固有ベクトルの累積寄与率
-    self.make_eigenvector_ticker(pca)
+    # #! 散布図
+    # self.make_scatter()
+
+    # #! 累積寄与率
+    # self.make_ticker(pca)
+    
+    # #! 固有ベクトルの散布図
+    # self.make_relations(pca)
+
+    # #! 固有ベクトルの累積寄与率
+    # self.make_eigenvector_ticker(pca)
+
+    #! 固有ベクトルの累積寄与率（x軸固定）
+    self.make_eigenvector_ticker(pca, lim=True)
 
 
   def main(self):
@@ -193,8 +222,8 @@ if __name__ == "__main__":
   date_format = today[2:4] + today[5:7] + today[8:10]
   #? >>>> ここは変更する >>>>
   input_file_name = "220325 調査報告書+IDs.xlsx"
-  output_file_name = date_format + "_result.xlsx"
-  output_chart_name = date_format + "_result"
+  output_file_name = date_format + "_集計.xlsx"
+  output_chart_name = date_format
   dir_names = ["04_pickup_params"]
   this_dir = 0                                    #! {0: 04_pickup_params}
   #? <<<< ここは変更する <<<<
